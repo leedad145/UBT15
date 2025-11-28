209c7ba5f0a7d6023bc842657b6667314ee21e8a
이재영 <76960587+leedad145@users.noreply.github.com>
2025-11-28 10:11:27 +0900

    aa

--- CSTest/Program.cs
+++ CSTest/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 //교과서
 //https://thebook.io/006890/
 //노션
 //https://aquatic-care-bc3.notion.site/Unity-15th-2b0715f54fd48008bcb4f25c2bb6f983
+//구글드라이브
+//https://drive.google.com/drive/folders/1xP_gRcH-dhrZyV2zBjc1f_eEOVFrEOrF?dmr=1&ec=wgc-drive-globalnav-goto
 class Program
 {
     static void Main()
@@ -14,6 +17,6 @@ class Program
         //RPS.RunTest();
         //StarTree.RunTest();
         //OddEven.RunTest();
-        
+
     }
 }
