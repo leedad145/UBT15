acde34e096d6a98f54401042087e370429c96934
이재영 <76960587+leedad145@users.noreply.github.com>
2025-12-04 16:30:35 +0900

    add Squ

--- Unity/My project/Assets/Scripts/Main.cs
+++ Unity/My project/Assets/Scripts/Main.cs
@@ -3,15 +3,9 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {//7개 생성 이름변경 스프라이트렌더러 스프라트드 스퀘어
-    public Sprite sprite;
-    Color[] colors = {Color.red, Color.orange, Color.yellow,  Color.green, Color.blue, Color.navyBlue, Color.purple};
     private void Start()
     { 
-        for (int i = 1;  i <= 7; i++)
-        {
-            CreateSquare("Square" + i, new Vector3(i - 4, 0, 0), colors[i-1]);
 
-        }
     }
 
     float sumTime = 0f; // 시간을 누적 시킬 변수
@@ -30,14 +24,5 @@ public class Main : MonoBehaviour
         GameObject go = new GameObject();
         Player player =  go.AddComponent<Player>();
     }
-    private void CreateSquare(string name, Vector3 pos, Color color)
-    { 
-        GameObject go = new GameObject();
-        SpriteRenderer sr = go.AddComponent<SpriteRenderer>();
-        sr.sprite = sprite;
-        sr.color = color;
-        go.name = name;
-        go.transform.position = pos;
 
-    }
 }
--- Unity/My project/Assets/Scripts/Square.cs
+++ Unity/My project/Assets/Scripts/Square.cs
@@ -1,20 +1,30 @@
 using UnityEngine;
 
 public class Square : MonoBehaviour
-{   
+{
+    public Sprite sprite;
+    Color[] colors = { Color.red, Color.orange, Color.yellow, Color.green, Color.blue, Color.navyBlue, Color.purple };
+
     void Start()
     {
-        GameObject go = gameObject;
-        gameObject.name = "";
-        gameObject.tag = "Player";
-        SpriteRenderer sr = go.AddComponent<SpriteRenderer>();
-        sr.color = Color.blue;
-
-        transform.position = new Vector3(2, 0, 0);
+        for (int i = 1; i <= 7; i++)
+        {
+            CreateSquare("Square" + i, new Vector3(i - 4, 0, 0), colors[i - 1]);
+        }
     }
 
     void Update()
     {
         
+    }
+    private void CreateSquare(string name, Vector3 pos, Color color)
+    {
+        GameObject go = new GameObject();
+        SpriteRenderer sr = go.AddComponent<SpriteRenderer>();
+        sr.sprite = sprite;
+        sr.color = color;
+        go.name = name;
+        go.transform.position = pos;
+
     }
 }
